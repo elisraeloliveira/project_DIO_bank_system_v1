c8f5c76f379ab7c7c5d540698db6d9b755910d8b
elisrael.oliveira <elisrael.oliveira@gmail.com>
2024-08-30 12:52:18 -0300

    commit v1 of banking system

--- banking_system_user_v1.py
+++ banking_system_user_v1.py
@@ -18,13 +18,51 @@ while True:
     opcao = input(menu)
 
     if opcao == "d":
-        print("Deposito")
+        deposito = float(input("Informe o valor de deposito: "))
+
+        if deposito > 0:
+            saldo += deposito
+            extrato += f"Deposito: R${deposito:.2f}\n"
+
+        else:
+            print(f"Operação falhou! O valor informado é invalido.")
+
 
     elif opcao == "s":
-        print("Sacar")
+        saque = float(input("Informe o valor de saque: "))
+
+        excedeu_saldo = saque > saldo
+
+        excedeu_limite = saque > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif excedeu_limite:
+            print("Operação falhou! O valor de saque excede o limite para saques\n O seu limite para saque é R$ 500.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número de saques excedido")
+
+        elif saque > 0:
+            saldo -= saque
+            extrato += f"Saque: R$: {saque:.2f}"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou: O valor informado é inválido")
+
 
     elif opcao == "e":
-        print("Extrato")
+        print("\n#################> EXTRATO <#################")
+        print("Não houve movimentaçõa em sua conta." if not extrato else extrato)
+        print(f"\n Saldo: R$: {saldo:.2f}")
+        print("##############################################")
+
+
+
 
     elif opcao == "q":
         print("Obrigado por usar nossos serviços.")
@@ -32,5 +70,8 @@ while True:
 
     else:
         print("Operação invalida, selecione por favor a opção desejada.")
+
+
+
 
 
